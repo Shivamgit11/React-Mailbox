69f581e0c8cdd758d10937fdca1db0997fde2486
shivam <mailtoshivam2002@gmail.com>
2022-12-24 23:48:24 +0530

    Deleting some useless files

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import "./App.css";
 import AuthPage from "./components/Pages/AuthPage";
 import { Redirect, Route } from "react-router-dom";
 
-import MailDetails from "./components/Mail/MailDetails";
 import ManagedMails from "./components/Mail/ManagedMails";
 
-import Showsentedmails from "./components/Mail/SentDetails";
-import { Button } from "react-bootstrap";
 
-// import AuthPage from './components/Pages/AuthPage';
-// import { Route, Switch } from "react-router-dom";
+
+
 
 function App() {
-  
   return (
     <main>
       <Route path="/" exact>
@@ -22,15 +18,8 @@ function App() {
       <Route path="/auth" exact>
         <AuthPage />
       </Route>
-      {/* <Route path="/inbox" exact>
-        <Inbox />
-      </Route> */}
-      <Route path="/inbox/details" exact>
-        <MailDetails />
-      </Route>
-      <Route path="/sent/details" exact>
-        <Showsentedmails />
-      </Route>
+      
+
       <Route path="/mail">
         {/* <Mailfirst /> */}
         <ManagedMails />
